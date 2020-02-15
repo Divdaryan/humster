28f50ac9be42258723e4e6beb965a2f4aa7e78ce
Narek <narekdivdaryan94@gmail.com>
2020-02-15 14:45:27 -0800

    init Project

---
+++ Scripts/Hamster.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hamster : MonoBehaviour
+{
+    [SerializeField] float speed = 4f;
+    [SerializeField] float jumpForce = 5f;
+
+    private bool letJump = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += new Vector3(Input.GetAxis("Horizontal") * Time.fixedDeltaTime * speed, 0f, 0f);
+        
+        if (Input.GetButtonDown("Vertical") && letJump)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,jumpForce), ForceMode2D.Impulse);
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground") {
+            letJump = true;
+        }
+        else if(collision.collider.tag == "Enamy")
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            letJump = false;
+        }
+    }
+
+}
